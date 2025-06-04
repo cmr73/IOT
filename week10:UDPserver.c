7b418b42ec4a567e276b3d3df63a216602dc07e0
Anonymous <112873721+cmr73@users.noreply.github.com>
2025-06-04 17:47:01 +0530

    Update week10:UDPserver.c

--- week10:UDPserver.c
+++ week10:UDPserver.c
@@ -4,44 +4,44 @@
 const char* ssid = “ak";
 const char* password = “12345678";
 const char* udpAddress = "192.168.68.144";
-const int udpPort = 1234;
+const int udpPort = 8081;
 #define DHTPIN D3 
-#define DHTTYPE DHT11  
+#define DHTTYPE DHT11  
 DHT dht(DHTPIN, DHTTYPE);
 WiFiUDP udp;
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting");
-  }
-  Serial.println();
-  Serial.println("Connected to WiFi.IP:");
-  dht.begin();
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting");
+  }
+  Serial.println();
+  Serial.println("Connected to WiFi.IP:");
+  dht.begin();
 }
 void loop() {
-  delay(10000);
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+  delay(10000);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
 Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" °C\tHumidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-  Serial.println("Sending data over UDP...");
-  udp.beginPacket(udpAddress, udpPort);
-  udp.print("Temperature: ");
-  udp.print(temperature);
-  udp.print(" °C, Humidity: ");
-  udp.print(humidity);
-  udp.println(" %");
-  udp.endPacket();
-  Serial.println("Data sent over UDP.");
+  Serial.print(temperature);
+  Serial.print(" °C\tHumidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  Serial.println("Sending data over UDP...");
+  udp.beginPacket(udpAddress, udpPort);
+  udp.print("Temperature: ");
+  udp.print(temperature);
+  udp.print(" °C, Humidity: ");
+  udp.print(humidity);
+  udp.println(" %");
+  udp.endPacket();
+  Serial.println("Data sent over UDP.");
 }
